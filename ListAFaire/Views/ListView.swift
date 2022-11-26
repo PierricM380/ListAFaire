42651f6dc91430f0ccc11eff96b01363f1779a07
PierricM380 <pierric.marye@wanadoo.fr>
2022-11-26 11:02:32 +0100

    Deleting edit method in navigation title

--- ListAFaire/Views/ListView.swift
+++ ListAFaire/Views/ListView.swift
@@ -34,7 +34,6 @@ struct ListView: View {
             }
         }
         .navigationBarItems(
-            leading: EditButton(),
             trailing: NavigationLink(
                 destination: AddView(),
                 label: {
